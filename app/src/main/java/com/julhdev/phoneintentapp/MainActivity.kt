9814453984859d2beb8c6b3eaceec697da92b023
Julieth Sarmiento <jasa1999@hotmail.com>
2025-11-05 11:25:06 -0500

    feat: proyecto iniciado

---
+++ app/src/main/java/com/julhdev/phoneintentapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.julhdev.phoneintentapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.julhdev.phoneintentapp.ui.theme.PhoneIntentAppTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    setContent {
+      PhoneIntentAppTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+          Greeting(
+            name = "Android",
+            modifier = Modifier.padding(innerPadding)
+          )
+        }
+      }
+    }
+  }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+  Text(
+    text = "Hello $name!",
+    modifier = modifier
+  )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+  PhoneIntentAppTheme {
+    Greeting("Android")
+  }
+}
